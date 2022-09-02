74ac0b7c979bf64fbba3fec79307d1f65b44a336
Erick Saito <saitoerick@gmail.com>
2022-09-01 23:55:33 -0300

    add invert and odd images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Implementation of matplotlib function
 import numpy as np
-from PIL import Image as img, ImageShow
+from PIL import Image, ImageShow
 import matplotlib.pyplot as plt
 from skimage import filters, exposure
   
@@ -15,27 +15,39 @@ from skimage import filters, exposure
 
 # Transformação de intensidade
 def intensity():
-  city_image = img.open('images/city.png')
+  city_image = Image.open('images/city.png')
+  city_image = np.array(city_image)
   negative_image = np.invert(city_image)
-  negative_image = img.fromarray(negative_image)
+  negative_image = Image.fromarray(negative_image)
   negative_image.save('results/intensity01.png')
+
+  odd_invert = Image.open('images/city.png')
+  odd_invert = np.array(odd_invert)
+  odd_invert[::2] = np.fliplr(odd_invert[::2])
+  odd_invert = Image.fromarray(odd_invert)
+  odd_invert.save('results/intensity03.png')
+  
+  mirror_img = Image.open('images/city.png') 
+  mirror_img = np.flipud(mirror_img)
+  mirror_img = Image.fromarray(mirror_img)
+  mirror_img.save('results/intensity05.png')
 
 # Ajuste de brilho
 def brightness():
-  baboon_image = plt.imread('images/baboon.png')
+  baboon_image = Image.open('images/baboon.png')
 
 # Planos de bits
 def bits_plan():
-  baboon_image = plt.imread('images/baboon.png')
+  baboon_image = Image.open('images/baboon.png')
 
 # Mosaico
 def mosaic():
-  baboon_image = plt.imread('images/baboon.png')
+  baboon_image = Image.open('images/baboon.png')
 
 # Combinação de imagens 
 def image_combinator():
-  baboon_image = plt.imread('images/baboon.png')
-  butterfly_image = plt.imread('images/butterfly.png')
+  baboon_image = Image.open('images/baboon.png')
+  butterfly_image = Image.open('images/butterfly.png')
 
 
 intensity()
